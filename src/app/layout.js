ccea16a570490886014f48f4faeae50929e74c6e
Gabe Morris <gabemorris1042@gmail.com>
2025-07-09 20:24:46 -0300

    path change in layout for vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "../app/components/Navbar";
-import Footer from "../app/components/Footer";
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import { Poppins } from 'next/font/google'
 
 const poppins = Poppins({
