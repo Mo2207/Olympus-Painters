b5e55035cf9de9df372eb50b476d254c7d8f2973
Gabe Morris <gabemorris1042@gmail.com>
2025-07-09 20:32:20 -0300

    Fix file casing for Navbar/Footer for Vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Navbar from './components/Navbar.js'
+import Footer from './components/Footer.js'
 import { Poppins } from 'next/font/google'
 
 const poppins = Poppins({
