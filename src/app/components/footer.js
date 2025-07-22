01763e696e9db65302e0a09258125121fe9ec273
Gabe Morris <gabemorris1042@gmail.com>
2025-07-22 20:06:50 -0300

    vercel is making me want to throw my computer at a wall

--- src/app/components/footer.js
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-  return (
-    <footer className="bg-[#235A81] text-gray-800 py-4 text-center">
-      © {new Date().getFullYear()} Olympus Painters. All rights reserved.
-    </footer>
-  );
-}
